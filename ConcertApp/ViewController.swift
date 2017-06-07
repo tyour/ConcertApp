649c136497cdabb20c5d699335452690d32c08d8
Thomas Your <20015508@ATC203-M26.local>
2017-06-06 18:55:28 -0700

    Added background

--- ConcertApp/ViewController.swift
+++ ConcertApp/ViewController.swift
@@ -5,21 +5,21 @@
 //  Copyright © 2017 Team2. All rights reserved.
 //
 
-//ViewController should be renamed
+//Main ViewController
 import UIKit
 
 class ViewController: UIViewController {
-//test change
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "concertbackground1x"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+//New test
 
 }
 
