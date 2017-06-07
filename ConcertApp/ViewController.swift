c9fb2ada2e5f7b4b9d841bfdc96c55a8260c0646
Thomas Your <Thomas@Thomass-MacBook-Air.local>
2017-06-07 13:49:37 -0700

    Attempt 2: Buttons and title added, additional formatting

--- ConcertApp/ViewController.swift
+++ ConcertApp/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
+    @IBOutlet var ArtistButton: UIButton!
+    @IBOutlet var EventButton: UIButton!
+    @IBOutlet var GenreButton: UIButton!
+    @IBOutlet var LocationButton: UIButton!
+    
+    @IBOutlet var TitleLabel: UILabel!
+    @IBOutlet var ButtonClusterBackground: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "concertbackground1x"))
+        
+        //Formats button cluster
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "concertbackground_formatted"))
+        ArtistButton.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "icon_artist"))
+        EventButton.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "icon_event"))
+        GenreButton.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "icon_genre"))
+        LocationButton.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "icon_location"))
+        
+        ArtistButton.layer.cornerRadius = 5
+        EventButton.layer.cornerRadius = 5
+        GenreButton.layer.cornerRadius = 5
+        LocationButton.layer.cornerRadius = 5
+        
+        
+        ButtonClusterBackground.layer.masksToBounds = true
+        ButtonClusterBackground.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
