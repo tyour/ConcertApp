385731e9a3294abf499d5529c78eacc7eb88a56f
Eric  Chung <echung505@atc203-m27.local>
2017-06-07 19:37:23 -0700

    Adjustments to ArtistTableViewController

--- ConcertApp/ArtistTableViewController.swift
+++ ConcertApp/ArtistTableViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ArtistTableViewController: UITableViewController {
+class ArtistTableViewController: UITableViewController, UISearchResultsUpdating {
 
-    var artists = ["Ariana Grande", "Justin Bieber", "Ed Sheeran", "Beyonce"]
+    var Artists = [ArtistsMO] = []
+    
+    var searchController : UISearchController
+    var searchResults : [ArtistsMO] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,23 +34,26 @@ class ArtistTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Artists.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cellIdentifier = "ArtistCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as!
+            ArtistTableViewCell
 
         // Configure the cell...
-
+        cell.ArtistName?.text = cellItem.iName
+        cell.textLabel?.text = Artists[indexPath.row]
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
