e4f4c8c85d841018aa11e551326cd9dd6235f6b5
samdeanefox <samdeanefox@gmail.com>
2017-06-10 17:58:59 -0700

    implemented json parsing

--- ConcertApp/ViewController.swift
+++ ConcertApp/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     func makeGetCall () {
-        let url_str = "http://api.jambase.com/events?zipCode=95110&radius=5&startDate=2017-06-20&endDate=2017-06-21&page=0&api_key=832t889gh3n8728fzxthr644"
+        let url_str = "http://api.jambase.com/events?zipCode=95110&radius=50&startDate=2017-06-20&endDate=2017-06-30&page=0&api_key=832t889gh3n8728fzxthr644"
         // Set up the URL request
         let todoEndpoint: String = url_str
         guard let url = URL(string: todoEndpoint) else {
@@ -26,10 +26,38 @@ class ViewController: UIViewController {
         
         // make the request
         let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
-            print("We are in the callback")
-            // do stuff with response, data & error here
-            print(error ?? "no error")
-            print(response ?? "no reponse")
+            // check for any errors
+            guard error == nil else {
+                print("error calling GET on /todos/1")
+                print(error)
+                return
+            }
+            // make sure we got data
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            // parse the result as JSON, since that's what the API provides
+            do {
+                guard let response_data = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject] else {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                // now we have the todo, let's just print it to prove we can access it
+                print("\(response_data["Events"])")
+                print("whatup")
+                
+                // the todo object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
+                //guard let todoTitle = response_data["title"] as? String else {
+                  //  print("Could not get todo title from JSON")
+                   // return
+                //}
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
         })
         print("hi")
         task.resume()
