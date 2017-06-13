84723836bdce9bb96084d8e0b11f971895800b33
Eric  Chung <echung505@atc203-m27.local>
2017-06-12 19:35:51 -0700

    Added ArtistTableViewCell, made adjustments to ArtistTableViewController

---
+++ ConcertApp/ArtistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistTableViewCell.swift
+//  ConcertApp
+//
+//  Created by Eric  Chung on 6/12/17.
+//  Copyright © 2017 Team2. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
