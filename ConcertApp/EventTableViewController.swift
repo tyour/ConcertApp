0ddfd43b2c2883eac3de478b9b8008b1bf51d404
David Bueno <david@Davids-Mac.local>
2017-06-13 23:32:44 -0700

    TableView now displays list of venues, city, state info

--- ConcertApp/EventTableViewController.swift
+++ ConcertApp/EventTableViewController.swift
@@ -21,6 +21,7 @@ class EventTableViewController: UITableViewController, UISearchResultsUpdating {
     var EventImage = #imageLiteral(resourceName: "icon_event")
     var fetchName: String = "\0"
     var fetchLocation: String = "\0"
+    var TableData:[String: AnyObject] = [:]
     
     //var searchResults = []
     
@@ -39,9 +40,9 @@ class EventTableViewController: UITableViewController, UISearchResultsUpdating {
         self.searchController.dimsBackgroundDuringPresentation = false
         self.tableView.tableHeaderView = self.searchController.searchBar
         //makeGetCall(input: "oracle+arena")
+        get_data_from_url(url: "http://api.jambase.com/venues?name=oracle&page=0&api_key=sbxzadxwszauykseun6pdj3u")
         
-        
-        makeGetCall()
+        //makeGetCall()
         //tableView.reloadData()
     }
 
@@ -59,7 +60,17 @@ class EventTableViewController: UITableViewController, UISearchResultsUpdating {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        
+        if TableData["Venues"]?.count != nil
+        {
+            return TableData["Venues"]!.count
+        }
+        
+        else
+        {
+            return 0
+        }
+        //return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->  UITableViewCell {
@@ -68,76 +79,131 @@ class EventTableViewController: UITableViewController, UISearchResultsUpdating {
         
         //if searchController.isActive {
         //let temp : String = makeGetCall(input: "oracle+arena")
-        cell.EventName.text = fetchName
-        cell.EventDate.text = fetchLocation
+        cell.EventName.text = (TableData["Venues"]![indexPath.row]! as! [String: Any])["Name"]! as? String
+        cell.EventDate.text = ((TableData["Venues"]![indexPath.row]! as! [String: Any])["City"]! as? String)! + ", " + ((TableData["Venues"]![indexPath.row]! as! [String: Any])["State"]! as? String)!
         cell.EventImage.image = UIImage(named: "icon_event")
         //}
         return cell
     }
+    
+    // http://api.jambase.com/venues?name=oracle+arena&page=0&api_key=sbxzadxwszauykseun6pdj3u&o=json
 
-    func makeGetCall() {
-        let url_str = "http://api.jambase.com/venues?name=oracle+arena&page=0&api_key=sbxzadxwszauykseun6pdj3u"
-        // Set up the URL request
-        let todoEndpoint: String = url_str
-        guard let url = URL(string: todoEndpoint) else {
-            print("Error: cannot create URL")
-            return
-        }
-        let urlRequest = URLRequest(url: url)
-        
-        // set up the session
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        // make the request
-        let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
-            // check for any errors
-            guard error == nil else {
-                print("error calling GET on /todos/1")
-                print(error)
-                return
-            }
-            // make sure we got data
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            // parse the result as JSON, since that's what the API provides
-            do {
-                guard let response_data = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject] else {
-                    print("error trying to convert data to JSON")
-                    return
+    func get_data_from_url(url:String)
+    {
+        let httpMethod = "GET"
+        let timeout = 15
+        let url = URL(string: url)
+        let urlRequest = URLRequest(url: url!,
+                                             cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                             timeoutInterval: 15.0)
+        let queue = OperationQueue()
+        NSURLConnection.sendAsynchronousRequest(
+            urlRequest,
+            queue: queue,
+            completionHandler: {(response, data, error) in
+                if (data?.count)! > 0 && error == nil{
+                    self.extract_json(jsonData: data!)
+                }else if data?.count == 0 && error == nil{
+                    print("Nothing was downloaded")
+                } else if error != nil{
+                    print("Error happened = \(error)")
                 }
-                // now we have the todo, let's just print it to prove we can access it
-                print("\((response_data["Venues"]![0]! as! [String: Any])["Name"]! as! String)")
-                self.fetchName = (response_data["Venues"]![0]! as! [String: Any])["Name"]! as! String
-                self.fetchLocation = ((response_data["Venues"]![0]! as! [String: Any])["City"]! as! String) + ", " + ((response_data["Venues"]![0]! as! [String: Any])["State"]! as! String)
-                self.tableView.beginUpdates()
-                self.tableView.deleteRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
-                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
-                self.tableView.endUpdates()
-
-                print("whatup")
-                
-                
-                
-                // the todo object is a dictionary
-                // so we just access the title using the "title" key
-                // so check for a title and print it if we have one
-                //guard let todoTitle = response_data["title"] as? String else {
-                //  print("Could not get todo title from JSON")
-                // return
-                //}
-            } catch  {
+        }
+        )
+    }
+    
+    func extract_json(jsonData:Data)
+    {
+        var parseError: NSError?
+        do {
+            guard let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: AnyObject] else {
                 print("error trying to convert data to JSON")
                 return
             }
-        })
-        print("hi")
-        task.resume()
-        
-        print("bye")
+            if (parseError == nil)
+            {
+                TableData = json
+                print(TableData)
+            }
+        } catch  {
+            print("error trying to convert data to JSON")
+            return
+        }
+
+
+        do_table_refresh();
     }
+
+    func do_table_refresh()
+    {
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+            return
+        })
+    }
+//    func makeGetCall() {
+//        let url_str = "http://api.jambase.com/venues?name=oracle+arena&page=0&api_key=sbxzadxwszauykseun6pdj3u"
+//        // Set up the URL request
+//        let todoEndpoint: String = url_str
+//        guard let url = URL(string: todoEndpoint) else {
+//            print("Error: cannot create URL")
+//            return
+//        }
+//        let urlRequest = URLRequest(url: url)
+//        
+//        // set up the session
+//        let config = URLSessionConfiguration.default
+//        let session = URLSession(configuration: config)
+//        
+//        // make the request
+//        let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+//            // check for any errors
+//            guard error == nil else {
+//                print("error calling GET on /todos/1")
+//                print(error)
+//                return
+//            }
+//            // make sure we got data
+//            guard let responseData = data else {
+//                print("Error: did not receive data")
+//                return
+//            }
+//            // parse the result as JSON, since that's what the API provides
+//            do {
+//                guard let response_data = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject] else {
+//                    print("error trying to convert data to JSON")
+//                    return
+//                }
+//                // now we have the todo, let's just print it to prove we can access it
+//                print("\((response_data["Venues"]![0]! as! [String: Any])["Name"]! as! String)")
+//                self.fetchName = (response_data["Venues"]![0]! as! [String: Any])["Name"]! as! String
+//                self.fetchLocation = ((response_data["Venues"]![0]! as! [String: Any])["City"]! as! String) + ", " + ((response_data["Venues"]![0]! as! [String: Any])["State"]! as! String)
+//                self.tableView.beginUpdates()
+//                self.tableView.deleteRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
+//                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
+//                self.tableView.endUpdates()
+//
+//                print("whatup")
+//                
+//                
+//                
+//                // the todo object is a dictionary
+//                // so we just access the title using the "title" key
+//                // so check for a title and print it if we have one
+//                //guard let todoTitle = response_data["title"] as? String else {
+//                //  print("Could not get todo title from JSON")
+//                // return
+//                //}
+//            } catch  {
+//                print("error trying to convert data to JSON")
+//                return
+//            }
+//        })
+//        print("hi")
+//        task.resume()
+//        
+//        print("bye")
+//    }
 
 
     override var prefersStatusBarHidden: Bool{
