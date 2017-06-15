9f86af783c91f3bcff3cfba9e05c7131a8ae0e5d
Eric  Chung <echung505@atc203-m27.local>
2017-06-14 19:50:56 -0700

    ArtistTableViewController updates

--- ConcertApp/ArtistTableViewController.swift
+++ ConcertApp/ArtistTableViewController.swift
@@ -14,6 +14,9 @@ class ArtistTableViewController: UITableViewController, UISearchResultsUpdating 
     var searchController : UISearchController!
     var artistName : [String] = ["ArtistName"]
     var artistImage : [UIImage] = [#imageLiteral(resourceName: "icon_artist")]
+    var fetchName: String = "\0"
+    var TableData:[String: AnyObject] = [:]
+    
     
     func updateSearchResults(for searchController: UISearchController) {
         tableView.reloadData()
@@ -35,6 +38,7 @@ class ArtistTableViewController: UITableViewController, UISearchResultsUpdating 
         self.searchController.searchResultsUpdater = self
         self.searchController.dimsBackgroundDuringPresentation = false
         self.tableView.tableHeaderView = self.searchController.searchBar
+        get_data_from_url(url: "http://api.jambase.com/events?artistId=12345")
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,11 +65,64 @@ class ArtistTableViewController: UITableViewController, UISearchResultsUpdating 
             ArtistTableViewCell
 
         // Configure the cell...
-        cell.ArtistName?.text = artistName[indexPath.row]
-        cell.ArtistImage?.image = artistImage[indexPath.row]
+        cell.ArtistName.text = (TableData["Artists"]![indexPath.row]! as! [String: Any])["Name"]! as? String
+        cell.ArtistImage.image = UIImage(named: "icon_artist")
         return cell
     }
 
+    func get_data_from_url(url:String)
+    {
+        let httpMethod = "GET"
+        let timeout = 15
+        let url = URL(string: url)
+        let urlRequest = URLRequest(url: url!,
+                                    cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                    timeoutInterval: 15.0)
+        let queue = OperationQueue()
+        NSURLConnection.sendAsynchronousRequest(
+            urlRequest,
+            queue: queue,
+            completionHandler: {(response, data, error) in
+                if (data?.count)! > 0 && error == nil{
+                    self.extract_json(jsonData: data!)
+                }else if data?.count == 0 && error == nil{
+                    print("Nothing was downloaded")
+                } else if error != nil{
+                    print("Error happened = \(error)")
+                }
+        }
+        )
+    }
+    
+    func extract_json(jsonData:Data)
+    {
+        var parseError: NSError?
+        do {
+            guard let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: AnyObject] else {
+                print("error trying to convert data to JSON")
+                return
+            }
+            if (parseError == nil)
+            {
+                TableData = json
+                print(TableData)
+            }
+        } catch  {
+            print("error trying to convert data to JSON")
+            return
+        }
+        
+        
+        do_table_refresh();
+    }
+    
+    func do_table_refresh()
+    {
+        DispatchQueue.main.async(execute: {
+            self.tableView.reloadData()
+            return
+        })
+    }
 
     /*
     // Override to support conditional editing of the table view.
