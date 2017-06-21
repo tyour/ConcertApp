9b6400889eaf78db5293349d2c465e9e09346d9c
Eric  Chung <echung505@atc203-m25.local>
2017-06-20 19:56:05 -0700

    DetailViewController (added button to bookmark), Created new Action for bookmark

--- ConcertApp/BookmarksTableViewController.swift
+++ ConcertApp/BookmarksTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BookmarksTableViewController: UITableViewController {
 
+    var MyBookmarks : [EventObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
